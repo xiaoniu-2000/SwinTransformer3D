d7c75a39bdcc70b35c5139942d213f41129f6cb6
xiaoniu <niuyao1103@gmail.com>
2026-01-13 16:47:11 +0800

    first commit

---
+++ transformer_layers.py
@@ -0,0 +1,397 @@
+'''
+Author: xiaoniu
+Date: 2026-01-10 18:35:25
+LastEditors: xiaoniu
+LastEditTime: 2026-01-12 23:46:38
+Description: 文件用途描述
+'''
+import torch
+import torch.nn as nn
+from shift_window_mask import window_partition,window_reverse,get_shift_window_mask
+from collections.abc import Sequence
+from utils import get_pad3d, crop3d
+from attention_layers import EarthAttention3D
+from drop import DropPath
+from mlp_layers import Mlp
+class UpSample3D(nn.Module):
+    """
+    Revise from WeatherLearn https://github.com/lizhuoq/WeatherLearn
+    3D Up-sampling operation.
+    Implementation from: https://github.com/198808xc/Pangu-Weather/blob/main/pseudocode.py
+
+    Args:
+        in_dim (int): Number of input channels.
+        out_dim (int): Number of output channels.
+        input_resolution (tuple[int]): [pressure levels, latitude, longitude]
+        output_resolution (tuple[int]): [pressure levels, latitude, longitude]
+    """
+
+    def __init__(self, in_dim, out_dim, input_resolution, output_resolution):
+        super().__init__()
+        self.linear1 = nn.Linear(in_dim, out_dim * 4, bias=False)
+        self.linear2 = nn.Linear(out_dim, out_dim, bias=False)
+        self.norm = nn.LayerNorm(out_dim)
+        self.input_resolution = input_resolution
+        self.output_resolution = output_resolution
+
+    def forward(self, x: torch.Tensor):
+        """
+        Args:
+            x (torch.Tensor): (B, N, C)
+        """
+        B, N, C = x.shape
+        in_pl, in_lat, in_lon = self.input_resolution
+        out_pl, out_lat, out_lon = self.output_resolution
+
+        x = self.linear1(x)
+        x = x.reshape(B, in_pl, in_lat, in_lon, 2, 2, C // 2).permute(
+            0, 1, 2, 4, 3, 5, 6
+        )
+        x = x.reshape(B, in_pl, in_lat * 2, in_lon * 2, -1)
+
+        pad_h = in_lat * 2 - out_lat
+        pad_w = in_lon * 2 - out_lon
+
+        pad_top = pad_h // 2
+        pad_bottom = pad_h - pad_top
+
+        pad_left = pad_w // 2
+        pad_right = pad_w - pad_left
+
+        x = x[
+            :,
+            :out_pl,
+            pad_top : 2 * in_lat - pad_bottom,
+            pad_left : 2 * in_lon - pad_right,
+            :,
+        ]
+        x = x.reshape(x.shape[0], x.shape[1] * x.shape[2] * x.shape[3], x.shape[4])
+        x = self.norm(x)
+        x = self.linear2(x)
+        return x
+
+class FuserLayer(nn.Module):
+    """Revise from WeatherLearn https://github.com/lizhuoq/WeatherLearn
+    A basic 3D Transformer layer for one stage
+
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resolution.
+        depth (int): Number of blocks.
+        num_heads (int): Number of attention heads.
+        window_size (tuple[int]): Local window size.
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0
+        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm
+    """
+
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        depth,
+        num_heads,
+        window_size,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.depth = depth
+
+        self.blocks = nn.ModuleList(
+            [
+                Transformer3DBlock(
+                    dim=dim,
+                    input_resolution=input_resolution,
+                    num_heads=num_heads,
+                    window_size=window_size,
+                    shift_size=(0, 0, 0) if i % 2 == 0 else None,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop,
+                    attn_drop=attn_drop,
+                    drop_path=drop_path[i]
+                    if isinstance(drop_path, Sequence)
+                    else drop_path,
+                    norm_layer=norm_layer,
+                )
+                for i in range(depth)
+            ]
+        )
+
+    def forward(self, x):
+        for blk in self.blocks:
+            x = blk(x)
+        return x
+    
+class UpSample3D(nn.Module):
+    """
+    Revise from WeatherLearn https://github.com/lizhuoq/WeatherLearn
+    3D Up-sampling operation.
+    Implementation from: https://github.com/198808xc/Pangu-Weather/blob/main/pseudocode.py
+
+    Args:
+        in_dim (int): Number of input channels.
+        out_dim (int): Number of output channels.
+        input_resolution (tuple[int]): [pressure levels, latitude, longitude]
+        output_resolution (tuple[int]): [pressure levels, latitude, longitude]
+    """
+
+    def __init__(self, in_dim, out_dim, input_resolution, output_resolution):
+        super().__init__()
+        self.linear1 = nn.Linear(in_dim, out_dim * 4, bias=False)
+        self.linear2 = nn.Linear(out_dim, out_dim, bias=False)
+        self.norm = nn.LayerNorm(out_dim)
+        self.input_resolution = input_resolution
+        self.output_resolution = output_resolution
+
+    def forward(self, x: torch.Tensor):
+        """
+        Args:
+            x (torch.Tensor): (B, N, C)
+        """
+        B, N, C = x.shape
+        in_pl, in_lat, in_lon = self.input_resolution
+        out_pl, out_lat, out_lon = self.output_resolution
+
+        x = self.linear1(x)
+        x = x.reshape(B, in_pl, in_lat, in_lon, 2, 2, C // 2).permute(
+            0, 1, 2, 4, 3, 5, 6
+        )
+        x = x.reshape(B, in_pl, in_lat * 2, in_lon * 2, -1)
+
+        pad_h = in_lat * 2 - out_lat
+        pad_w = in_lon * 2 - out_lon
+
+        pad_top = pad_h // 2
+        pad_bottom = pad_h - pad_top
+
+        pad_left = pad_w // 2
+        pad_right = pad_w - pad_left
+
+        x = x[
+            :,
+            :out_pl,
+            pad_top : 2 * in_lat - pad_bottom,
+            pad_left : 2 * in_lon - pad_right,
+            :,
+        ]
+        x = x.reshape(x.shape[0], x.shape[1] * x.shape[2] * x.shape[3], x.shape[4])
+        x = self.norm(x)
+        x = self.linear2(x)
+        return x
+
+class Transformer3DBlock(nn.Module):
+    """
+    Revise from WeatherLearn https://github.com/lizhuoq/WeatherLearn
+    3D Transformer Block
+    Args:
+        dim (int): Number of input channels.
+        input_resolution (tuple[int]): Input resulotion.
+        num_heads (int): Number of attention heads.
+        window_size (tuple[int]): Window size [pressure levels, latitude, longitude].
+        shift_size (tuple[int]): Shift size for SW-MSA [pressure levels, latitude, longitude].
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float, optional): Stochastic depth rate. Default: 0.0
+        act_layer (nn.Module, optional): Activation layer. Default: nn.GELU
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+    """
+
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        num_heads,
+        window_size=None,
+        shift_size=None,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        window_size = (2, 6, 12) if window_size is None else window_size
+        shift_size = (1, 3, 6) if shift_size is None else shift_size
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.shift_size = shift_size
+        self.mlp_ratio = mlp_ratio
+
+        self.norm1 = norm_layer(dim)
+        padding = get_pad3d(input_resolution, window_size)
+        self.pad = nn.ZeroPad3d(padding)
+
+        pad_resolution = list(input_resolution)
+        pad_resolution[0] += padding[-1] + padding[-2]
+        pad_resolution[1] += padding[2] + padding[3]
+        pad_resolution[2] += padding[0] + padding[1]
+
+        self.attn = EarthAttention3D(
+            dim=dim,
+            input_resolution=pad_resolution,
+            window_size=window_size,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            act_layer=act_layer,
+            drop=drop,
+        )
+
+        shift_pl, shift_lat, shift_lon = self.shift_size
+        self.roll = shift_pl and shift_lon and shift_lat
+
+        if self.roll:
+            attn_mask = get_shift_window_mask(pad_resolution, window_size, shift_size)  #(num_lon, num_pl*num_lat, Wpl*Wlat*Wlon, Wpl*Wlat*Wlon)=(30,31x4,2x6x12,2x6x12)
+        else:
+            attn_mask = None
+
+        self.register_buffer("attn_mask", attn_mask)
+
+    def forward(self, x: torch.Tensor):
+        Pl, Lat, Lon = self.input_resolution
+        B, L, C = x.shape
+
+        shortcut = x
+        x = self.norm1(x)
+        x = x.view(B, Pl, Lat, Lon, C)
+
+        # start pad
+        x = self.pad(x.permute(0, 4, 1, 2, 3)).permute(0, 2, 3, 4, 1) #(B,C,8,186,360) -> (B,8,186,360,C)
+
+        _, Pl_pad, Lat_pad, Lon_pad, _ = x.shape
+
+        shift_pl, shift_lat, shift_lon = self.shift_size
+        if self.roll:
+            shifted_x = torch.roll(
+                x, shifts=(-shift_pl, -shift_lat, -shift_lon), dims=(1, 2, 3)
+            )
+            x_windows = window_partition(shifted_x, self.window_size)
+            # B*num_lon, num_pl*num_lat, win_pl, win_lat, win_lon, C    # (B*360/12, (8/2)*(186/6),2,6,12,C)=(B*30,4*31,2,6,12,C)
+        else:
+            shifted_x = x
+            x_windows = window_partition(shifted_x, self.window_size)
+            # B*num_lon, num_pl*num_lat, win_pl, win_lat, win_lon, C
+
+        win_pl, win_lat, win_lon = self.window_size
+        x_windows = x_windows.view(
+            x_windows.shape[0], x_windows.shape[1], win_pl * win_lat * win_lon, C
+        )
+        # B*num_lon, num_pl*num_lat, win_pl*win_lat*win_lon, C # (B*30,4*31,2*6*12,C)
+
+        attn_windows = self.attn(
+            x_windows, mask=self.attn_mask
+        )  # B*num_lon, num_pl*num_lat, win_pl*win_lat*win_lon, C
+
+        attn_windows = attn_windows.view(
+            attn_windows.shape[0], attn_windows.shape[1], win_pl, win_lat, win_lon, C
+        )
+
+        if self.roll:
+            shifted_x = window_reverse(
+                attn_windows, self.window_size, Pl=Pl_pad, Lat=Lat_pad, Lon=Lon_pad
+            )
+            # B * Pl * Lat * Lon * C    #(B,8,186,360,C)
+            x = torch.roll(
+                shifted_x, shifts=(shift_pl, shift_lat, shift_lon), dims=(1, 2, 3)
+            )
+        else:
+            shifted_x = window_reverse(
+                attn_windows, self.window_size, Pl=Pl_pad, Lat=Lat_pad, Lon=Lon_pad
+            )
+            x = shifted_x
+
+        # crop, end pad
+        x = crop3d(x.permute(0, 4, 1, 2, 3), self.input_resolution).permute(
+            0, 2, 3, 4, 1
+        )
+
+        x = x.reshape(B, Pl * Lat * Lon, C)
+        x = shortcut + self.drop_path(x)
+
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+
+        return x
+
+class DownSample3D(nn.Module):
+    """
+    Revise from WeatherLearn https://github.com/lizhuoq/WeatherLearn
+    3D Down-sampling operation
+    Implementation from: https://github.com/198808xc/Pangu-Weather/blob/main/pseudocode.py
+
+    Args:
+        in_dim (int): Number of input channels.
+        input_resolution (tuple[int]): [pressure levels, latitude, longitude]
+        output_resolution (tuple[int]): [pressure levels, latitude, longitude]
+    """
+
+    def __init__(self, in_dim, input_resolution, output_resolution):
+        super().__init__()
+        self.linear = nn.Linear(in_dim * 4, in_dim * 2, bias=False)
+        self.norm = nn.LayerNorm(4 * in_dim)
+        self.input_resolution = input_resolution
+        self.output_resolution = output_resolution
+
+        in_pl, in_lat, in_lon = self.input_resolution
+        out_pl, out_lat, out_lon = self.output_resolution
+
+        h_pad = out_lat * 2 - in_lat
+        w_pad = out_lon * 2 - in_lon
+
+        pad_top = h_pad // 2
+        pad_bottom = h_pad - pad_top
+
+        pad_left = w_pad // 2
+        pad_right = w_pad - pad_left
+
+        pad_front = pad_back = 0
+
+        self.pad = nn.ZeroPad3d(
+            (pad_left, pad_right, pad_top, pad_bottom, pad_front, pad_back)
+        )
+
+    def forward(self, x):
+        B, N, C = x.shape
+        in_pl, in_lat, in_lon = self.input_resolution
+        out_pl, out_lat, out_lon = self.output_resolution
+        x = x.reshape(B, in_pl, in_lat, in_lon, C)
+
+        # Padding the input to facilitate downsampling
+        x = self.pad(x.permute(0, -1, 1, 2, 3)).permute(0, 2, 3, 4, 1)
+        x = x.reshape(B, in_pl, out_lat, 2, out_lon, 2, C).permute(0, 1, 2, 4, 3, 5, 6)
+        x = x.reshape(B, out_pl * out_lat * out_lon, 4 * C)
+
+        x = self.norm(x)
+        x = self.linear(x)
+        return x
